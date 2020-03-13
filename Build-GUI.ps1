dc20ad816cfbec8a774cb4d02910bee532fc7e53
Art Barnes <abarnes@lanl.gov>
2020-03-13 12:16:34 -0600

    remove shell scripts

--- Build-GUI.ps1
+++
@@ -1,5 +0,0 @@
-$InstallPath = $HOME\AppData\Local\Executables
-
-if (-not (Test-Path $InstallPath)) { mkdir $InstallPath }
-
-pyinstaller -wF pylinegui.py
--- Install-GUI.ps1
+++
@@ -1,6 +0,0 @@
-$InstallPath = $HOME\AppData\Local\Executables
-
-if (-not (Test-Path $InstallPath)) { mkdir $InstallPath }
-
-pyinstaller -wF pylinegui.py
-copy dist\pylinegui.exe $InstallPath
